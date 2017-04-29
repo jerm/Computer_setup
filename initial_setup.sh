216a070fe5cd71e3be9ec7830fa24624f9cc4548
Jeremy Price <github@jerm.org>
2017-04-29 01:50:16 -0700

    removing some cruft and ignoring some errors -- packages that complain if they're already installed

--- initial_setup.sh
+++ initial_setup.sh
@@ -25,22 +25,6 @@ brew install ansible
 #pip install --user ansible
 sudo mkdir -p /etc/ansible/hosts && sudo chown -R $USER /etc/ansible
 export ANSIBLE_NOCOWS=1
-#open http://getmacapps.com/
-#open https://www.hipchat.com/downloads
-# appstore: mirosoft remote desktop, tweetbot, hashtab
-#open http://www.crashplan.com
-#open http://ridiculousfish.com/hexfiend/
-#open http://www.kekaosx.com/en/
-#open http://tapbots.com/software/tweetbot/mac/
-#open http://macromates.com/download
-#open http://www.teamviewer.com/en/download/mac.aspx
-#open http://bjango.com/mac/istatmenus/
-#open https://www.keepassx.org/
-#open https://xquartz.macosforge.org/landing/
-#open http://wiki.mikrotik.com/wiki/MikroTik_WinBox_for_Mac_StandAlone
-
-## (not needed for winbox anymore) winebottler http://winebottler.kronenberg.org/downloads
-# remember http://rogueamoeba.com/  for audio tools
 ansible-playbook -i localhost.inv osx.yml --ask-sudo-pass
 
 [[ "$hacks" == "y" ]] && ./osx-hacks.sh
